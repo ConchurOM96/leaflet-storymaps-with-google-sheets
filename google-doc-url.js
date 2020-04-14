be7ca27d6737524bfb1dc2c058c558a84e27f720
ConchurOM96 <63465537+ConchurOM96@users.noreply.github.com>
2020-04-14 14:04:54 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ccG-OZtOxy65i_LsrADC8E2lrWSz7acHwmjEPoqfqSQ/edit#gid=0';
